94cab51b57915a40f38cbf8811ebe68b2fbccd66
ivocruznetto <cruzivo@live.com>
2021-06-29 20:26:27 -0300

    Day5

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -36,8 +36,8 @@ export function NewRoom() {
     <div id="page-auth">
       <aside>
         <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
-        <strong>Crie salas de Q&amp;A ao-vivo</strong>
-        <p>Tire as dúvidas da sua audiência em tempo-real</p>
+        <strong>Crie salas de Q&amp;A ao vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo real</p>
       </aside>
       <main>
         <div className="main-content">
